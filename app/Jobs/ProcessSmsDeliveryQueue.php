2343d461f67e36103a00bfe70ce0aa6a60cfa3ee
DevMaurice <developermaurice@gmail.com>
2022-01-29 10:31:00 +0300

    Process delivery messages - WIP

---
+++ app/Jobs/ProcessSmsDeliveryQueue.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Message;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class ProcessSmsDeliveryQueue implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $request;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+         Log::info('On queueueueueueueu');
+        Log::info($this->request);
+        dd();
+        $message = Message::where('tracking_code',$this->request['message_id'])->first();
+
+        $message->delivered_at = Carbon::parse();
+
+        $message->update();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [SendSmsController::class,'send']);
+Route::post('/webhook', [SendSmsController::class,'delivery']);
