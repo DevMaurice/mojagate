62f64b1071d33192f8b7f8b92563011ddad8740d
DevMaurice <developermaurice@gmail.com>
2022-01-29 09:21:10 +0300

    Send SMS to number.

---
+++ app/Http/Controllers/SendSmsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Moja;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class SendSmsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function send(Request $request)
+    {
+        
+        $result = Moja::send('254714692255','Message test','12345test');
+
+        
+
+        //save data
+    }
+}
